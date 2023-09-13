dd68d67da51eb414b8470789e7263fc13df10dc4
burkeev <burkeev2018@yandex.ru>
2023-09-13 11:05:13 +0300

    ended 2.1

---
+++ src/com/java_homework/practice_2/practice_2_1/TestAuthor.java
@@ -0,0 +1,18 @@
+package com.java_homework.practice_2.practice_2_1;
+import java.util.Scanner;
+public class TestAuthor {
+    public static void main(String[] args) {
+        String email;
+        // we need to use functions from dz2.Author class
+        Author k = new Author("John", "john@me.com", 'M');
+        System.out.println("Name: " + k.getName());
+
+        Scanner source = new Scanner(System.in);
+        System.out.println("What's your email? ");
+        email = source.nextLine();
+        k.setEmail(email);
+
+        System.out.println("Email: " + k.getEmail());
+        System.out.println("Gender: " + k.getGender());
+    }
+}
